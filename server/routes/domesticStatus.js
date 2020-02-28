c5ee18edffd587b22b1d2c7f4e3185ebf7e26646
OhSeungBeen <osb1992@naver.com>
2020-02-28 17:37:16 +0900

    dashbord template , 국내현황, 도시별현황

--- server/routes/domesticStatus.js
+++ server/routes/domesticStatus.js
@@ -16,10 +16,42 @@ router.get('/status', function(req, res, next) {
   getHtml().then(html =>{
     var domesticStatus ={};
     const $ = cheerio.load(html.data);
-    domesticStatus.confirmator = $('.s_listin_dot').children().eq(0).text().replace(/[^0-9]/g,'');
-    domesticStatus.isolate = $('.s_listin_dot').children().eq(1).text().replace(/[^0-9]/g,'');
-    domesticStatus.dead = $('.s_listin_dot').children().eq(2).text().replace(/[^0-9]/g,'');
-    domesticStatus.inspection = $('.s_listin_dot').children().eq(3).text().replace(/[^0-9]/g,'');
+    domesticStatus.confirmator = $('.w_bold').eq(0).text();
+    domesticStatus.isolate = $('.w_bold').eq(1).text()
+    domesticStatus.dead = $('.w_bold').eq(2).text()
+    domesticStatus.inspection = $('.w_bold').eq(3).text()
+    res.json(domesticStatus);
+  })
+});
+
+router.get('/statusByCity', function(req, res, next) {
+  
+  const getHtml = async() => {
+    try{
+      return await axios.get("http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun=");
+    } catch(error){
+      console.log.error(error);
+    }
+  }
+
+  getHtml().then(html =>{
+    var domesticStatus = new Array();
+    const $ = cheerio.load(html.data);
+    var i = 1;
+    while($("td[headers='status_con s_type1']").eq(i).text() != ""){
+      if($("td[headers='status_con s_type1']").eq(i).text().trim() == "0"){
+        i++
+        continue;
+      }
+      var statusByCity = {};
+      statusByCity.city = $("th[scope='row']").eq(i).text();
+      statusByCity.numbers = Number($("td[headers='status_con s_type1']").eq(i).text().trim().replace(',',''));
+      domesticStatus.push(statusByCity)
+      i++;
+    }
+    domesticStatus.sort(function(a, b) {
+      return b["numbers"] - a["numbers"];
+  });
     res.json(domesticStatus);
   })
 });
