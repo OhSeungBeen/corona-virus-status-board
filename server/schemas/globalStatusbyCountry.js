e1926adf1c2b71fa763aa614a8a7e449336b8dc9
OhSeungBeen <osb1992@naver.com>
2020-03-16 19:13:32 +0900

    Delete globalStatusbyCountry.js

--- server/schemas/globalStatusbyCountry.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-let globalStatusbyCountrySchema = new Schema({}, { strict: false });
-module.exports = mongoose.model('globalStatusbyCountry', globalStatusbyCountrySchema);
