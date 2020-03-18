0dde05a1bd1d3d7425cde64253c77ecbd967e153
OhSeungBeen <osb1992@naver.com>
2020-03-18 23:04:49 +0900

    국내 시도별 지도, 확진환자, 격리해제 사망자 추가

--- server/schedule/globalStatusByCountry.js
+++ server/schedule/globalStatusByCountry.js
@@ -6,42 +6,42 @@ moment.tz.setDefault('Asia/Seoul');
 const GlobalStatusByCountry = require('../schemas/globalStatusByCountry');
 
 module.exports = () => {
-  //  schedule.scheduleJob('*/30 * * * *', function() {
-  try {
-    (async () => {
-      const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-      const page = await browser.newPage();
-      await page.goto('https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6');
-      await page.waitForSelector('h5');
-      const result = await page.evaluate(() => {
-        const hTags = document.querySelectorAll('h5');
-        result = {};
-        for (let i = 0; i < hTags.length; i++) {
-          let country = hTags[i].children[2].textContent;
-          let confirmator = hTags[i].children[0].children[0].textContent.replace(/[^0-9]/g, '');
-          result[country] = { confirmator };
-        }
+  schedule.scheduleJob('*/30 * * * *', function() {
+    try {
+      (async () => {
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        const page = await browser.newPage();
+        await page.goto('https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6');
+        await page.waitForSelector('h5');
+        const result = await page.evaluate(() => {
+          const hTags = document.querySelectorAll('h5');
+          result = {};
+          for (let i = 0; i < hTags.length; i++) {
+            let number = hTags[i].children[0].children[0].textContent.replace(/[^0-9]/g, '');
+            let country = hTags[i].children[2].textContent;
+            result[country] = number;
+          }
 
-        return result;
-      });
-      await browser.close();
-
-      result.date = moment().format('YYYY-MM-DD HH:mm:ss');
-      let globalStatusbyCountry = new GlobalStatusByCountry(result);
-      globalStatusbyCountry
-        .save()
-        .then(result => {
-          logger.info('globalStatusByCountry DB 저장');
-          logger.info(result);
-        })
-        .catch(error => {
-          logger.error('globalStatusByCountry DB 저장실패');
-          logger.error(error);
+          return result;
         });
-    })();
-  } catch (err) {
-    logger.error('globalStatusByCountry crolling || DB 저장실패');
-    logger.error(err);
-  }
-  //  });
+        await browser.close();
+
+        result.date = moment().format('YYYY-MM-DD HH:mm:ss');
+        let globalStatusbyCountry = new GlobalStatusByCountry(result);
+        globalStatusbyCountry
+          .save()
+          .then(result => {
+            logger.info('globalStatusByCountry DB 저장');
+            logger.info(result);
+          })
+          .catch(error => {
+            logger.error('globalStatusByCountry DB 저장실패');
+            logger.error(error);
+          });
+      })();
+    } catch (err) {
+      logger.error('globalStatusByCountry crolling || DB 저장실패');
+      logger.error(err);
+    }
+  });
 };
